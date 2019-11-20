2404b271565fb19819cf6a9c26992f100e3a0d5c
borjaeg <borjaesgon1998@gmail.com>
2019-11-20 09:56:41 +0100

    :tada: commit inicial

---
+++ src/org/japo/java/app/App.java
@@ -0,0 +1,44 @@
+/* 
+ * Copyright 2019 Mario Merlos Abella - mario.merlos.alum@iescamp.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.app;
+
+//import java.util.Random;
+import org.japo.java.libraries.UtilesEntrada;
+import org.japo.java.libraries.UtilesPrimitivos;
+
+/**
+ *
+ * @author Mario Merlos Abella - mario.merlos.alum@iescamp.es
+ */
+public final class App {
+
+    
+    
+    public final void launchApp() {
+        //Variable
+        System.out.println("NÚMERO DE LA SUERTE");
+        System.out.println("===================");
+        int n = UtilesEntrada.leerEntero(
+                "Tu número de la suerte es:",
+                "Error: Entrada incorrecta"
+        );
+
+        System.out.printf("Tu número es ...: %s%n", UtilesPrimitivos.validarPar(n)?"PAR":"IMPAR");
+        System.out.printf("Tu número es ...: %s%n", UtilesPrimitivos.validarPos(n)?"POSITIVO":"NEGATIVO");
+
+    }
+
+}
